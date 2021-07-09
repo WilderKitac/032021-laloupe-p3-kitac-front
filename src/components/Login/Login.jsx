4c261bc18b7917e190db0f5e40250f653ede4dfd
Damien Landouard <dlandouard@gmail.com>
2021-07-09 10:14:12 +0200

    ajout du JWT et login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -26,28 +26,31 @@ export default function Login({ refreshToken }) {
           dispatch({ type: 'SET_USER', user: data.user });
           dispatch({ type: 'SET_JWT', jwt: data.token });
           dispatch({ type: 'SET_ROLE', jwt: data.role });
-          history.push('/');
+          if (data.token !== undefined) {
+            history.push('/');
+          } else {
+            alert('l&aposemail ou le mot de passe est erroné');
+          }
         })
         .catch((err) => {
-          // console.log(err);
-          alert(err.data);
+          alert(err.response.data);
         });
     } else {
-      alert('You must provide all information');
+      alert('Vous devez fournir toutes les informations');
     }
   };
 
   return (
     <form className="form" onSubmit={(event) => handleSubmit(event)}>
-      <h2>Login Page</h2>
+      <h2>Page de connexion</h2>
       <label htmlFor="email" className="form__label">
         Email : <input type="email" id="email" className="form__input" value={email} onChange={(event) => setEmail(event.target.value)} />
       </label>
       <label htmlFor="password" className="form__label">
-        Password :{' '}
+        Mot de passe :{' '}
         <input type="password" id="password" className="form__input" value={password} onChange={(event) => setPassword(event.target.value)} />
       </label>
-      <input className="button" type="submit" value="Login" />
+      <input className="button" type="submit" value="Se connecter" />
     </form>
   );
 }
