f0485593e559f04c2ac9cf2744d53b6cabfad09b
nqueudot <nqueudot28@gmail.com>
2021-07-12 12:12:12 +0200

    avencer sur la page accueil

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,17 +15,17 @@ function Home() {
   // console.log(categories);
 
   return (
-    <div>
-      <section className="backroundHome">
-        <section className="encadreTitre">
+    <section>
+      <div className="backroundHome">
+        <div className="encadreTitre">
           <h1 className="titreHome">TITRE</h1>
           <h2 className="titreHome">Sous Titre</h2>
           <p className="titreHome">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever
             since the 1500s
           </p>
-        </section>
-        <section className="encadreShop">
+        </div>
+        <div className="encadreShop">
           <h1 className="titleShop">LA BOUTIK A KITS</h1>
           <Carousel showThumbs={false} infiniteLoop={true} autoPlay>
             {categories?.map((item) => (
@@ -37,9 +37,44 @@ function Home() {
               </>
             ))}
           </Carousel>
-        </section>
-      </section>
-    </div>
+        </div>
+      </div>
+      <div className="h_Texte">
+        <h1>Apprendre a coudre avec Kitac</h1>
+        <h2>Un concept original</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since
+          the 1500s.
+        </p>
+      </div>
+      <div className="h_ImageHome">
+        <img alt="imageHome" src="https://picsum.photos/150/100" />
+        <img alt="imageHome2" src="https://picsum.photos/150/100" />
+      </div>
+      <div className="h_Bouton">
+        <button className="ps_cart_button">Je me lance</button>
+      </div>
+
+      <div className="h_Texte">
+        <h1>L'EQUIPE</h1>
+        <h2>Sous-tire</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since
+          the 1500s.
+        </p>
+      </div>
+      <div className="h_EncadreCrea">
+        <img className="h_imgCreatrice" alt="Angélique" src="./src/img/Angelique.png" />
+        <img className="h_imgCreatrice" alt="Valérie" src="./src/img/Valerie.png" />
+      </div>
+      <div className="h_Bouton">
+        <button className="ps_cart_button">Faisons connaissance</button>
+      </div>
+      <div className="h_NewsLetter">
+        <p>Restez informés des nouveautés en aveant-première.</p>
+        <p>Promis, On va pas vous spammer !</p>
+      </div>
+    </section>
   );
 }
 
