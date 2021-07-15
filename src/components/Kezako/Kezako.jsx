1cbb7609d2fd13db95077f12f88f1413847c4762
alexander1147 <abbas.alexander@hotmail.com>
2021-07-15 11:35:45 +0200

    container kezako

--- src/components/Kezako/Kezako.jsx
+++ src/components/Kezako/Kezako.jsx
@@ -6,13 +6,13 @@ function Kezako() {
     <section className="body">
       <div>
         <img className="grandePhoto" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
-        <h3>Kitac Kezako</h3>
+        <h3 className="text">Kitac Kezako</h3>
       </div>
-      <div>
+      <div className="container">
         <article>
           <img className="photoKezako" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
-          <h3>Le concept</h3>
-          <p>
+          <h3 className="text">Le concept</h3>
+          <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime facere consequatur iusto expedita blanditiis quaerat quibusdam
             fugiat dolorem similique animi, repellendus eum ullam id tenetur dolores? Aperiam, libero blanditiis?
           </p>
@@ -20,8 +20,8 @@ function Kezako() {
 
         <article>
           <img className="photoKezako" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
-          <h3>L'équipe</h3>
-          <p>
+          <h3 className="text">L'équipe</h3>
+          <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime facere consequatur iusto expedita blanditiis quaerat quibusdam
             fugiat dolorem similique animi, repellendus eum ullam id tenetur dolores? Aperiam, libero blanditiis?
           </p>
@@ -29,8 +29,8 @@ function Kezako() {
 
         <article>
           <img className="photoKezako" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
-          <h3>Contact</h3>
-          <p>
+          <h3 className="text">Contact</h3>
+          <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime facere consequatur iusto expedita blanditiis quaerat quibusdam
             fugiat dolorem similique animi, repellendus eum ullam id tenetur dolores? Aperiam, libero blanditiis?
           </p>
@@ -38,8 +38,8 @@ function Kezako() {
 
         <article>
           <img className="photoKezako" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
-          <h3>Collaborer avec Kitac</h3>
-          <p>
+          <h3 className="text">Collaborer avec Kitac</h3>
+          <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime facere consequatur iusto expedita blanditiis quaerat quibusdam
             fugiat dolorem similique animi, repellendus eum ullam id tenetur dolores? Aperiam, libero blanditiis?
           </p>
