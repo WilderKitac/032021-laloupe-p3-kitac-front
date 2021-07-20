84803bfc1f22b7359524f71458f51545b9246fc1
alexander1147 <abbas.alexander@hotmail.com>
2021-07-20 11:25:56 +0200

    mise a jour

--- src/components/Admin/AdminUserCreate.jsx
+++ src/components/Admin/AdminUserCreate.jsx
@@ -77,42 +77,42 @@ export default function AdminUserCreate() {
               <div>
                 <label htmlFor="firstname">
                   Prénom:
-                  <Field name="firstname" id="firstname" />
+                  <Field className="field" name="firstname" id="firstname" />
                   <ErrorMessage name="firstname" component="div" />
                 </label>
                 <label htmlFor="lastname">
                   Nom:
-                  <Field name="lastname" id="lastname" />
+                  <Field className="field" name="lastname" id="lastname" />
                   <ErrorMessage name="lastname" component="div" />
                 </label>
                 <label htmlFor="email">
                   Email:
-                  <Field name="email" id="email" />
+                  <Field className="field" name="email" id="email" />
                   <ErrorMessage name="email" component="div" />
                 </label>
                 <label htmlFor="password">
                   Mot de passe:
-                  <Field name="password" id="password" />
+                  <Field className="field" name="password" id="password" />
                   <ErrorMessage name="password" component="div" />
                 </label>
                 <label htmlFor="phone">
                   Numéro de téléphone:
-                  <Field name="phone" id="phone" />
+                  <Field className="field" name="phone" id="phone" />
                   <ErrorMessage name="phone" component="div" />
                 </label>
                 <label htmlFor="rue">
                   Rue:
-                  <Field name="rue" id="rue" />
+                  <Field className="field" name="rue" id="rue" />
                   <ErrorMessage name="rue" component="div" />
                 </label>
                 <label htmlFor="codePostal">
                   Code Postal:
-                  <Field name="codePostal" id="codePostal" />
+                  <Field className="field" name="codePostal" id="codePostal" />
                   <ErrorMessage name="codePostal" component="div" />
                 </label>
                 <label htmlFor="ville">
                   Ville/Commune:
-                  <Field name="ville" id="ville" />
+                  <Field className="field" name="ville" id="ville" />
                   <ErrorMessage name="ville" component="div" />
                 </label>
               </div>
