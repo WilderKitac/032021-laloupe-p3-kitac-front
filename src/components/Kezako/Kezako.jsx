a71ece01dc9dd488a4f319c8478ae7d9230b7624
alexander1147 <abbas.alexander@hotmail.com>
2021-07-26 11:18:57 +0200

    creation fenetres modal

--- src/components/Kezako/Kezako.jsx
+++ src/components/Kezako/Kezako.jsx
@@ -5,18 +5,16 @@ function Kezako() {
   return (
     <section className="body">
       <div className="zoom">
-        
-          <a href="http://localhost:3000/shop">
-            <img
-              className="grandePhoto"
-              src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg"
-              alt="photo"
-              onClick="http://localhost:3000/shop"
-            />
-          </a>
-        
-        <h3 className="text">Kitac Kezako</h3>
+        <a href="http://localhost:3000/shop">
+          <img
+            className="grandePhoto"
+            src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg"
+            alt="photo"
+            onClick="http://localhost:3000/shop"
+          />
+        </a>
       </div>
+      <h3 className="text">Kitac Kezako</h3>
       <div className="container">
         <article>
           <img className="photoKezako" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
@@ -44,15 +42,6 @@ function Kezako() {
             onClick="http://localhost:3000/Contact"
           />
           <h3 className="text">Contact</h3>
-          <p className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime facere consequatur iusto expedita blanditiis quaerat quibusdam
-            fugiat dolorem similique animi, repellendus eum ullam id tenetur dolores? Aperiam, libero blanditiis?
-          </p>
-        </article>
-
-        <article>
-          <img className="photoKezako" src="https://bucket.mlcdn.com/a/3123/3123652/images/575af8a8a85f73d0cf1a0fec08f2380cdf1d956e.jpeg" alt="" />
-          <h3 className="text">Collaborer avec Kitac</h3>
           <p className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus maxime facere consequatur iusto expedita blanditiis quaerat quibusdam
             fugiat dolorem similique animi, repellendus eum ullam id tenetur dolores? Aperiam, libero blanditiis?
