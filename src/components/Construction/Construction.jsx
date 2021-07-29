18f3acc99b5c38055260a71fb4dff04c545c793d
alexander1147 <abbas.alexander@hotmail.com>
2021-07-29 14:38:49 +0200

    creation component en construction

---
+++ src/components/Construction/Construction.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Construction.css';
+
+function Construction() {
+  return (
+    <section>
+      <div>
+        <img className="constructionPhoto" src="https://images.assetsdelivery.com/compings_v2/vanatchanan/vanatchanan1510/vanatchanan151000047.jpg" alt="en construction" />
+      </div>
+    </section>
+  );
+}
+
+export default Construction;
