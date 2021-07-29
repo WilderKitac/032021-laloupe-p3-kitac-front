4c6baf7bec70225f953c68b638a9be8b6e6991c7
alexander1147 <abbas.alexander@hotmail.com>
2021-07-29 15:27:25 +0200

    mise a jour css construction

--- src/components/Construction/Construction.jsx
+++ src/components/Construction/Construction.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Nav from '../Nav/Nav';
+
 import './Construction.css';
 
 function Construction() {
   return (
-    <section>
-      <div>
-        <img className="constructionPhoto" src="https://images.assetsdelivery.com/compings_v2/vanatchanan/vanatchanan1510/vanatchanan151000047.jpg" alt="en construction" />
-      </div>
+    <section className="construction_container">
+      <h2>Page en construction</h2>
+      <img
+        className="constructionPhoto"
+        src="https://cninfotech.com/wp-content/uploads/2019/08/Isle-of-Man-Website-Maintenace.jpg"
+        alt="en construction"
+      />
     </section>
   );
 }
